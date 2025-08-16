26c8c13c0fb21b21626675216382e56db8292f90
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-08-16 22:36:14 +0500

    logout updated

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -24,6 +24,7 @@ export function Profile() {
       localStorage.removeItem("token");
       localStorage.removeItem("user");
       toast.success("Logout successful!");
+      window.location.href = "/";
 
       
     } catch (error) {
